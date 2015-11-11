1cdfc46875843c295d8d9c59f604bba40f5a215c
Charles <charles.barbier.web@gmail.com>
2015-11-11 01:53:30 +0100

    rectif'

--- mail.php
+++ mail.php
@@ -10,13 +10,13 @@ if(isset($_POST['submit'])){
   }elseif($_POST['choixSerie'] == 'bb'){
     $body = file_get_contents('Archive/email_bb.html');
   }elseif($_POST['choixSerie'] == 'dxt'){
-    $body = file_get_contents('Archive/email_dxt.html');
+    $body = file_get_contents('Archive/email_dexter.html');
   }elseif($_POST['choixSerie'] == 'dn'){
     $body = file_get_contents('Archive/email_dn.html');
   }elseif($_POST['choixSerie'] == 'ment'){
-    $body = file_get_contents('Archive/email_ment.html');
+    $body = file_get_contents('Archive/email_mentalist.html');
   }elseif($_POST['choixSerie'] == 'mr'){
-    $body = file_get_contents('Archive/email_mr.html');
+    $body = file_get_contents('Archive/email_mrobot.html');
   }elseif($_POST['choixSerie'] == 'pll'){
     $body = file_get_contents('Archive/email_pll.html');
   }else{
@@ -33,14 +33,16 @@ if(isset($_POST['submit'])){
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'nospoilwithlove@gmail.com';                // SMTP username
     $mail->Password = 'jorbencha';                  // SMTP password
-    $mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
+    $mail->SMTPSecure = 'ssl';
+                             // Enable encryption, 'ssl' also accepted
 
     $mail->From = 'nospoilwithlove@gmail.com';
     $mail->FromName = '(Spoil) With Love';
     $mail->AddAddress($dest_mail);  // Add a recipient
                    // Name is optional
 
-    $mail->IsHTML(true);                                  // Set email format to HTML
+    $mail->IsHTML(true);
+    $mail->CharSet = 'UTF-8';                                 // Set email format to HTML
 
     $mail->Subject = 'Petit spoil d\'un ami';
     $mail->Body    = $body;
