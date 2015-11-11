402a0f440207b480e8ddb6f46a69c5505d2b157e
Charles <charles.barbier.web@gmail.com>
2015-11-11 15:42:36 +0100

    fb

--- mail.php
+++ mail.php
@@ -1,10 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
-
 if(isset($_POST['submit'])){
-
   if($_POST['choixSerie'] == 'got'){
-    $body = 'test';
+    $body = file_get_contents('Archive/email_got.html');
   }elseif($_POST['choixSerie'] == 'twd'){
     $body = file_get_contents('Archive/email_twd.html');
   }elseif($_POST['choixSerie'] == 'bb'){
@@ -22,11 +20,8 @@ if(isset($_POST['submit'])){
   }else{
     $body = file_get_contents('Archive/email_sons.html');
   }
-
     $dest_mail = htmlspecialchars(trim($_POST['email']));
-
     $mail = new PHPMailer;
-
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'ssl0.ovh.net';                 // Specify main and backup server
     $mail->Port = 465;                                    // Set the SMTP port
@@ -35,25 +30,19 @@ if(isset($_POST['submit'])){
     $mail->Password = 'jorchaben';                  // SMTP password
     $mail->SMTPSecure = 'ssl';
                              // Enable encryption, 'ssl' also accepted
-
     $mail->From = 'spoilwithlove@spoilwithlove.fr';
-    $mail->FromName = 'Test';
+    $mail->FromName = 'Un ami t\'envoi ceci';
     $mail->AddAddress($dest_mail);  // Add a recipient
                    // Name is optional
-
     $mail->IsHTML(true);
     $mail->CharSet = 'UTF-8';                                 // Set email format to HTML
-
     $mail->Subject = 'Petit spoil d\'un ami';
     $mail->Body    = $body;
     $mail->AltBody = 'Tu viens de recevoir un spoil d\'un pote!  Malheuresement ta boite mail ne peut pas lire le message! Va faire un tour sur http://spoilwithlove.fr !';
-
     if(!$mail->Send()) {
      header('Location: error.html');
      exit;
     }
     header('Location: success.html');
-
   }
-
 ?>
